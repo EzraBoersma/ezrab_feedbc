4f6a6c2ccb9434a4f2236ff19b29e15f8d5cf319
EzraBoersma <ezra.boersma@itas.ca>
2023-02-09 00:02:09 -0800

    Map prototype added

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,147 @@
-let map;
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  const markerArray = [];
+  // Instantiate a directions service.
+  const directionsService = new google.maps.DirectionsService();
+  // Create a map and center it on Manhattan.
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: { lat: 40.771, lng: -73.974 },
+  });
+  // Create a renderer for directions and bind it to the map.
+  const directionsRenderer = new google.maps.DirectionsRenderer({ map: map });
+  // Instantiate an info window to hold step text.
+  const stepDisplay = new google.maps.InfoWindow();
+
+  // Display the route between the initial start and end selections.
+  calculateAndDisplayRoute(
+    directionsRenderer,
+    directionsService,
+    markerArray,
+    stepDisplay,
+    map
+  );
+
+  // Listen to change events from the start and end lists.
+  const onChangeHandler = function () {
+    calculateAndDisplayRoute(
+      directionsRenderer,
+      directionsService,
+      markerArray,
+      stepDisplay,
+      map
+    );
+  };
+
+  document.getElementById("start").addEventListener("change", onChangeHandler);
+  document.getElementById("end").addEventListener("change", onChangeHandler);
+}
+
+function calculateAndDisplayRoute(
+  directionsRenderer,
+  directionsService,
+  markerArray,
+  stepDisplay,
+  map
+) {
+  // First, remove any existing markers from the map.
+  for (let i = 0; i < markerArray.length; i++) {
+    markerArray[i].setMap(null);
+  }
+
+  // Retrieve the start and end locations and create a DirectionsRequest using
+  // DRIVING directions.
+  directionsService
+    .route({
+      origin: document.getElementById("start").value,
+      destination: document.getElementById("end").value,
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    .then((result) => {
+      // Route the directions and pass the response to a function to create
+      // markers for each step.
+      document.getElementById("warnings-panel").innerHTML =
+        "<b>" + result.routes[0].warnings + "</b>";
+      directionsRenderer.setDirections(result);
+      showSteps(result, markerArray, stepDisplay, map);
+    })
+    .catch((e) => {
+      window.alert("Directions request failed due to " + e);
+    });
+}
+
+/* function showSteps(directionResult, markerArray, stepDisplay, map) {
+  // For each step, place a marker, and add the text to the marker's infowindow.
+  // Also attach the marker to an array so we can keep track of it and remove it
+  // when calculating new routes.
+  const myRoute = directionResult.routes[0].legs[0];
+
+  for (let i = 0; i < myRoute.steps.length; i++) {
+    const marker = (markerArray[i] =
+      markerArray[i] || new google.maps.Marker());
+
+    marker.setMap(map);
+    marker.setPosition(myRoute.steps[i].start_location);
+    attachInstructionText(
+      stepDisplay,
+      marker,
+      myRoute.steps[i].instructions,
+      map
+    );
+  }
+} */
+
+function attachInstructionText(stepDisplay, marker, text, map) {
+  google.maps.event.addListener(marker, "click", () => {
+    // Open an info window when the marker is clicked on, containing the text
+    // of the step.
+    stepDisplay.setContent(text);
+    stepDisplay.open(map, marker);
+  });
+}
+
+function attachInstructionText(stepDisplay, marker, text, map) {
+  google.maps.event.addListener(marker, "click", () => {
+    // Open an info window when the marker is clicked on, containing the text
+    // of the step.
+    stepDisplay.setContent(text);
+    stepDisplay.open(map, marker);
   });
 }
 
 window.initMap = initMap;
+// --------------------------------------------------
+
+//declare under map
+const contentString =
+  '<div id="content">' +
+  '<div id="siteNotice">' +
+  "</div>" +
+  '<h1 id="firstHeading" class="firstHeading">Fredrich\'s Honey</h1>' +
+  "<img";
+'<div id="bodyContent">' +
+  "<p>Located in the picturesque Cedar, British Columbia, just 20 minutes south of Nanaimo," +
+  "the Fredrich's Honey farm has been in operation since 1966, passed down through the generations." +
+  "We believe that the key to producing the finest honey lies in the care and maintenance of our hives. " +
+  "That's why we use only the most natural and sustainable methods in our beekeeping operations. " +
+  "From the materials we use to construct our hives, to the methods we employ to care for our bees, " +
+  "every aspect of our operation is designed to promote the health and well-being of our bees.</p>" +
+  "</div>" +
+  "</div>";
+
+const infowindow = new google.maps.InfoWindow({
+  content: contentString,
+  ariaLabel: "Fredrich's Honey",
+});
+
+const marker = new google.maps.Marker({
+  position: pointFredrichs,
+  map,
+  title: "Fredrich's Honey",
+});
+
+marker.addListener("click", () => {
+  infowindow.open({
+    anchor: marker,
+    map,
+  });
+});
