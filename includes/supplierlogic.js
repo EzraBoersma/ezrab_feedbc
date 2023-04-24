84ebfb7ca4e72d39f652388912ec0fedb45d091d
EzraBoersma <ezra.boersma@itas.ca>
2023-04-24 09:23:33 -0700

    Apr24 Daily Presenter

--- includes/supplierlogic.js
+++ includes/supplierlogic.js
@@ -1,106 +1,42 @@
-var markerCount = 0;
+   
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand">
+                <img src="./images/navbackground/feedBC.png" height="28" alt="Feed BC: Bringing BC foods to public institutions">
+            </a>
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navBar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+      <div class="collapse navbar-collapse" aria-expanded="false" id="navBar">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link active" href="index.html">Home
+            </a>
+          </li>
 
-for (var counter = 0; counter < supplier_list.length; counter++) {
-  // while there are items in the array, do:
-  var cell_data = supplier_list[count].split(",");
-  //split the current index into the separated values for this entry
-  for (var cell_count = 0; cell_count < cell_data.length; cell_count++) {
-    //While there is still cells of data available for this item, do:
+          <li class="nav-item">
+            <a class="nav-link" id="about_btn_nav">About</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true"
+              aria-expanded="false">View Menu</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" id="menu_btn_nav_greenmountain">Green Mountain</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" id="menu_btn_nav_theroost">Mt. Benson Market</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" id="menu_btn_nav_arrowsmith">The Roost</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" id="menu_btn_nav_mtbenson">Arrowsmith Grill</a>
 
-    var cell = cell_data[cell_count];
+            </div>
+          </li>
+        </ul>
+      </div>
+        </div>
+</nav>
     
-        if (cell_count === 0) {
-          //Manipulate the first cell of data differently [the first iteration]
-        } else {
-          // Manipulate the rest of the data afterwards [subsequent iterations]
-        }
+
+
 
     
-    //Decision structure
-    //[set to detect first item, and behave differently for this iteration]
-
-    if (cell_count === 0) {
-      //Manipulate the first cell of data differently [the first iteration]
-    } else {
-      // Manipulate the rest of the data afterwards [subsequent iterations]
-    }
-  } //This is where we close tags for the  CELL_DATA iterations.
-
-  markerCount++;
-} //This is where we close tags for the  SUPPLIER_LIST iterations.
-
-
-
-
-
-
-                        var icounter = 0;
-                        var writeString = false; //allow quotation-enclosed strings to be written
-                        var currSupplier;
-
-                        for (supplier_data in supplier) { // SPLIT THE SUPPLIER INTO ITS DATA PIECES FOR PROCESSING
-                            icounter++; // index tracker
-                            console.log(icounter); //);
-                            //console.log('This is entry ' + icounter + ', which contains data: ' + supplier_data);
-
-
-                            /* currSupplier = supplier_data[icounter].split(",");
-                            console.log(currSupplier.values()); */
-                            /*
-                            Check processed code for identifier?
-                            if (supplier_data.values().includes('"')) {
-                                writeString = !writeString; //
-                            }
-                             */
-
-                          
-                          
-                          
-                                                      if (supplier_data == "NA" || "") {
-
-                                console.log(`FAILED: This data field is blank: ${supplier}`);
-                                console.log('FAILED: This data field is blank: ' + supplier_data);
-
-                                //Manipulate the first cell of data differently [the first iteration]
-                            } else {
-                                // Manipulate the rest of the data afterwards [subsequent iterations]
-
-                                /*                                 console.log(`SUCCESS: This data field is populated: ${supplier}`);
-                                                                console.log('SUCCESS: This data field is populated: ' + supplier_data); */
-                            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
